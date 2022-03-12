203a8415cf4464d28d9f2102066ced63ff268a5e
Fiona <fiona.roux@hetic.net>
2022-03-12 23:00:31 +0100

    fix: try fix height hero on mobile devices (2)

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import { Blobs } from '@/components/organisms/Blobs';
 export const Hero = () => {
   // FIXME: Review style hero
   return (
-    <div className="z-20 flex flex-col items-center justify-center h-full px-4 overflow-hidden text-center lg:py-16 md:items-end md:text-right md:px-8 lg:px-20 bg-midnight">
+    <div className="z-20 flex flex-col items-center justify-center h-screen min-h-screen px-4 overflow-hidden text-center lg:py-16 md:items-end md:text-right md:px-8 lg:px-20 bg-midnight">
       <Blobs />
       <h2 className="font-black uppercase select-none text-[10vw] lg:text-[8vh] md:text-[8vw] xl:text-[8vh] font-brother text-shadow text-stroke-purple-ultraLight md:text-stroke-2 md:text-fill-transparent opacity-20 md:opacity-40">
         Performance
