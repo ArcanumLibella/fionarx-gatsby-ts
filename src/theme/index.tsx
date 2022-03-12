6e98a339416f5f96da57b9d78efbc74dd6eadc2b
Fiona <fiona.roux@hetic.net>
2022-03-12 14:44:19 +0100

    fix: error with theme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,18 +1,17 @@
-// import * as React from "react";
+import * as React from 'react';
+
 // import { ThemeProvider } from "styled-components";
 
 // const theme = {
 //   // TODO: Placeholder for future use
 // };
 
-// type ThemeProps = {
-//   children: React.ReactNode;
-// };
+type ThemeProps = {
+  children: React.ReactNode;
+};
 
-// const ThemeProvider = ({ children }: ThemeProps): JSX.Element => (
-//   <ThemeProvider theme={theme}>
-//     {children}
-//   </ThemeProvider>
-// );
+const ThemeProvider = ({ children }: ThemeProps): JSX.Element => (
+  <ThemeProvider /* theme={theme} */>{children}</ThemeProvider>
+);
 
-// export default ThemeProvider;
+export default ThemeProvider;
