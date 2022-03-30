9c94142747fbca5d5674a5127c38a13adb5e54af
ArcanumLibella <fiona.roux@hetic.net>
2022-03-30 20:21:46 +0200

    style: fix responsive blob on mobile device

--- src/components/organisms/Hero/Hero.tsx
+++ src/components/organisms/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   // FIXME: Review style hero
   return (
     <div className="z-20 flex flex-col items-center justify-center h-screen min-h-screen px-4 overflow-hidden text-center lg:py-16 md:items-end md:text-right md:px-8 lg:px-20 bg-midnight">
-      <Blobs className="absolute flex items-center justify-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[56vw] h-[40vh] lg:w-[48vw] lg:h-[54vh] xl:w-[32vw] xl:h-[56vh]" />
+      <Blobs className="absolute flex items-center justify-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[56vw] h-[36vh] lg:w-[48vw] lg:h-[54vh] xl:w-[32vw] xl:h-[56vh]" />
       <Text type="heroTitle">Performance</Text>
       <Text type="heroTitle">SEO</Text>
       <Text type="heroTitle">Back-office</Text>
--- src/layouts/SecondaryLayout.tsx
+++ src/layouts/SecondaryLayout.tsx
@@ -16,10 +16,10 @@ const SecondaryLayout = ({
 }: SecondaryLayoutProps) => {
   return (
     <div
-      className={`${additionalStyle} relative px-4 py-16 md:py-0 md:px-0 md:top-1/4 md:-right-1/3 3xl:-right-2/5 max-w-fit z-10`}
+      className={`${additionalStyle} relative px-4 pt-32 md:py-0 pb-16 md:px-0 md:top-1/4 md:-right-1/3 3xl:-right-2/5 max-w-fit z-10`}
     >
-      <Blobs className="hidden fixed md:flex justify-center items-center xl:-left-1/10 xl:-bottom-1/5 w-[56vw] h-[40vh] lg:w-[48vw] lg:h-[54vh] xl:w-[40vw] xl:h-[64vh]" />
-      <BlobsLight className="hidden fixed md:flex justify-center items-center xl:-right-1/10 xl:-top-1/5 w-[56vw] h-[40vh] lg:w-[48vw] lg:h-[54vh] xl:w-[40vw] xl:h-[72vh]" />
+      <Blobs className="fixed flex justify-center items-center -top-1/5 -right-1/10 xl:right-auto xl:top-auto xl:-left-1/10 xl:-bottom-1/5 w-[56vw] h-[36vh] lg:w-[48vw] lg:h-[54vh] xl:w-[40vw] xl:h-[64vh]" />
+      <BlobsLight className="hidden fixed xl:flex justify-center items-center xl:-right-1/10 xl:-top-1/5 w-[56vw] h-[40vh] lg:w-[48vw] lg:h-[54vh] xl:w-[40vw] xl:h-[72vh]" />
       <div className="max-w-800 2xl:max-w-5xl">
         <Text type="mainTitle" className="mb-10 text-tomato">
           {title}
