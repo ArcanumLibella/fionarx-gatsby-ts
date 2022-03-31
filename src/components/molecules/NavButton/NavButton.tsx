911e13067ea63bfaede93478455bdafcb9dcb1c9
Fiona <fiona.roux@hetic.net>
2022-03-31 16:39:16 +0200

    feat: add burger menu on desktop

--- src/components/molecules/NavButton/NavButton.tsx
+++ src/components/molecules/NavButton/NavButton.tsx
@@ -1,5 +1,8 @@
+import { useIsMobile } from "@/utils/useWindowSize";
 import { Link } from "gatsby";
 
+import { Squash as Hamburger } from "hamburger-react";
+import { useState } from "react";
 import { SocialIcons } from "../SocialIcons";
 
 type NavButtonProps = {
@@ -7,6 +10,9 @@ type NavButtonProps = {
 };
 
 export const NavButton = ({ toggleMenu }: NavButtonProps) => {
+  const [isOpen, setOpen] = useState(false);
+  const isMobile = useIsMobile();
+
   return (
     <div className="fixed flex-col items-center justify-between bottom-2 right-2 md:bottom-0 md:left-0 md:w-20 md:h-screen md:flex z-100">
       <Link
@@ -21,8 +27,12 @@ export const NavButton = ({ toggleMenu }: NavButtonProps) => {
         aria-hidden="true"
         className="flex-col items-center justify-between md:py-4 md:w-20 md:h-screen md:flex md:bg-purple-ultraDark"
       >
-        <span className="flex items-center justify-center p-2 font-bold border-2 rounded-full font-brother w-14 h-14 text-tiny md:justify-start md:items-start md:flex-1 border-purple-light md:border-none">
-          MENU
+        <span
+          className={`flex items-center justify-center p-2 font-bold border-2 rounded-full lg:p-0 font-brother text-tiny md:justify-start md:items-start md:flex-1 border-purple-light md:border-none
+            ${isMobile && "w-14 h-14"}
+          `}
+        >
+          {isMobile ? "MENU" : <Hamburger toggled={isOpen} toggle={setOpen} />}
         </span>
         <SocialIcons className="flex-col justify-end flex-1 hidden gap-3 md:flex" />
       </div>
